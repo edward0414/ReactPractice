4c55cc7279184a850b18e3a5fa77d435aa553dc2
Edward Feng <edward0414@gmail.com>
2018-05-09 13:16:22 -0400

    Set up starter code

---
+++ src/square.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Square extends React.Component {
+    
+    render() {
+        return(
+            <button className="square">
+            </button>
+        );
+    }
+}
+
+export default Square;
